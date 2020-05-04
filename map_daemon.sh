0c9cc74a72227377d3d7e43b0b76a96781252fca
Dimitar Dimitrov <newbrict@gmail.com>
2020-05-04 13:38:24 -0400

    added error checking + backup date for deleted maps

--- map_daemon.sh
+++ map_daemon.sh
@@ -6,10 +6,14 @@ output=/root/96/defrag-maps/defrag/
 
 while true; do
     latest_map=$(cat $latest_file)
-    new_latest_map=$(/usr/bin/python3 $scraper -o $output -p $latest_map | grep DONE | head -n1 | perl -pe "s/.* ([^.]*)...DONE\!/\1/g")
-    if [ -n "$new_latest_map" ]; then
-        echo "Latest map downloaded: $new_latest_map"
-        echo $new_latest_map > $latest_file
+    backup_date=$(date -d "-5 days" --iso)
+    new_latest_map=$(/usr/bin/python3 $scraper -o $output -p $latest_map -d $backup_date | grep DONE | head -n1 | perl -pe "s/.* ([^.]*)...DONE\!/\1/g")
+    # If we get a non zero return then just sleep and try again in 30s
+    if [ $? -eq 0 ]; then
+        if [ -n "$new_latest_map" ]; then
+            echo "Latest map downloaded: $new_latest_map"
+            echo $new_latest_map > $latest_file
+        fi
     fi
     sleep 30
 done
